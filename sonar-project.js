c21c271822f1d28d20db1f956dfe852cdf9038a2
volodymyr-coax-dev <volodymyr.sen@coaxsoft.com>
2021-10-23 21:36:19 +0300

    work with sonarqube

--- sonar-project.js
+++ sonar-project.js
@@ -2,11 +2,9 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
 serverUrl: 'http://64.227.116.189:9000',
 options: {
-  'sonar.projectKey': 'besvova490-home_pdp-store-next',
   'sonar.login': 'admin',
   'sonar.password': 'root',
-  'sonar.sources': '.',
-  'sonar.tests': '.',
+  'sonar.exclusions': 'node_modules/**,**/*.spec.ts,dist/**,**/*.js,coverage/**,',
   'sonar.inclusions': '**/*.{js,jsx,ts,tsx}', // Entry point of your code
   },
 }, () => {
