627bb270ef9104fdcb19250b1cac0253dc4a1678
volodymyr-coax-dev <volodymyr.sen@coaxsoft.com>
2021-10-23 22:16:56 +0300

    moved sonar password and login to the secrets

--- sonar-project.js
+++ sonar-project.js
@@ -2,8 +2,8 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
   serverUrl: 'http://64.227.116.189:9000',
   options: {
-    'sonar.login': secrets.SONAR_LOGIN,
-    'sonar.password': secrets.SONAR_PASSWORD,
+    'sonar.login': process.env.NEXT_APP_SONAR_LOGIN,
+    'sonar.password':  process.env.NEXT_APP_SONAR_PASSWORD,
     },
   }, () => {
   console.log('Error Occurred while scanning');
